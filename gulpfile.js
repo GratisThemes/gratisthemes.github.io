84afd7e26788474af9236735f1f43f004bad022c
Kniffen <knifftech@gmail.com>
2021-07-24 07:37:24 +0200

    2.0 initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,80 @@
-const gulp = require('gulp')
-const plumber = require('gulp-plumber')
-const jade = require('gulp-jade')
-const sass = require('gulp-sass')
-const prefix = require('gulp-autoprefixer')
-const webserver = require('gulp-webserver')
-const wait = require('gulp-wait')
+const gulp       = require('gulp')
+const plumber    = require('gulp-plumber')
+const sass       = require('gulp-sass')(require('sass'))
+const prefix     = require('gulp-autoprefixer')
+const handlebars = require('gulp-compile-handlebars')
+const rename     = require('gulp-rename')
+const liveServer = require("live-server")
 
-gulp.task('jade', () => {
-  gulp.src('./source/jade/**/!(_)*.jade')
+const pkg = require('./package.json')
+
+const options = {
+  batch: [
+    './src/html/partials'
+  ]
+}
+
+function html() {
+  return gulp.src('./src/html/pages/**/*.html')
     .pipe(plumber())
-    .pipe(jade({
-      locals: {},
-      pretty: true
+    .pipe(handlebars(
+      {
+        version: pkg.version
+      },
+      {
+        batch: [
+          './src/html/partials'
+        ],
+        helpers: {
+          isEqual: function(a, b, opts) {
+            return (a == b) ? opts.fn(this) : opts.inverse(this)
+          },
+          listItems: function(...items) {
+            const options = items.pop()
+            return items
+          }
+        }
+      }
+    ))
+    .pipe(rename(function(path) {
+      if ( path.basename !== 'index' ) {
+        path.dirname += `/${path.basename}`
+        path.basename = 'index'
+      }
     }))
-    .pipe(gulp.dest('./'))
-})
+    .pipe(gulp.dest('./docs'))
+}
 
-gulp.task('sass', () => {
-  gulp.src('./source/scss/**/*.scss')
+function css() {
+  return gulp.src('./src/scss/*.scss')
     .pipe(plumber())
-    .pipe(wait(500))
-    .pipe(sass({ outputStyle: 'expanded', includePaths: ['scss'] }))
+    .pipe(sass({ outputStyle: 'expanded', includePaths: ['scss'] }).on('error', sass.logError))
     .pipe(prefix(['last 30 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-    .pipe(gulp.dest('./'))
-})
+    .pipe(gulp.dest('./docs'))
+}
 
-gulp.task('webserver', () => {
-  gulp.src('./')
+function static() {
+  return gulp.src('./src/static/**/*')
     .pipe(plumber())
-    .pipe(webserver({
-      livereload: true,
-      directoryListing: false,
-      open: true
-    }))
-})
+    .pipe(gulp.dest('./docs'))
+}
 
-gulp.task('watch', () => {
-  gulp.watch('./source/jade/**/*.*', ['jade'])
-  gulp.watch('./source/scss/**/*.*', ['sass'])
-})
+function serve(done) {
+  liveServer.start({
+    root: './docs',
+    open: false,
+  })
+  done()
+}
 
-gulp.task('default', ['watch', 'jade', 'sass', 'webserver'])
+function watch() {
+  gulp.watch('src/html/**/*',      {cwd: './', usePolling: true}, html)
+  gulp.watch('src/scss/**/*.scss', {cwd: './', usePolling: true}, css)
+  gulp.watch('src/static/**/*',    {cwd: './', usePolling: true}, static)
+}
+
+module.exports.html   = html
+module.exports.css    = css
+module.exports.static = static
+module.exports.dev    = gulp.series(html, css, static, serve, watch)
+module.exports.build  = gulp.series(html, css, static, watch)
